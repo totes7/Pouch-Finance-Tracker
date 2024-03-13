997b6d90ae0c44c2dda180f4cda82e2a1098e65f
unicorn453 <kira.ruseva@gmail.com>
2024-03-13 23:04:19 +0200

    added Transactions component and styling

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import dayjs from 'dayjs';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from './Form';
+import React from "react";
+import dayjs from "dayjs";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./Form";
+import Transactions from "./Transactions";
 
 const Overview = () => {
-  const userName = 'John';
-  const currentDate = dayjs().format('ddd D MMMM YYYY');
+  const userName = "John";
+  const currentDate = dayjs().format("ddd D MMMM YYYY");
 
   return (
     <div className="container">
-    <div className="container-fluid bg-light p-3">
-      <div className="row">
-        <div className="col">
-          <p className="m-0">Welcome back, {userName}!</p>
-        </div>
-        <div className="col text-end">
-          <p className="m-0">{currentDate}</p>
+      <div className="container-fluid bg-light p-3">
+        <div className="row">
+          <div className="col">
+            <p className="m-0">Welcome back, {userName}!</p>
+          </div>
+          <div className="col text-end">
+            <p className="m-0">{currentDate}</p>
+          </div>
         </div>
       </div>
-    </div>
       <Form />
+      <Transactions />
     </div>
   );
 };
