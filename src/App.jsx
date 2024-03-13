d6bc783ffcdc9bef6736fa297869302d294876c8
nolythedev <nolychigwedere@hotmail.co.uk>
2024-03-13 20:17:58 +0000

    Overview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AccountAccess from './components/AccountAccess';
-import Form from './components/Form'
+
+import Overview from './components/Overview';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Router>
+        <Routes>
+        <Route exact path="/" element={<Overview />} /> {/* Define a route for the Overview component */}
+        </Routes>
+      </Router>
       {/* <AccountAccess /> */}
-      {/* <Form /> */}
+   
     </>
   )
 }
