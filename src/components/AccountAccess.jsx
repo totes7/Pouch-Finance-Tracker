71ba5a9fe6775f03b4c1db3e7211d380ddfbd7a7
udani521 <udani.karunratne@gmail.com>
2024-03-13 19:10:59 +0000

    added overview bar

--- src/components/AccountAccess.jsx
+++ src/components/AccountAccess.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios'; // Import axios for making HTTP requests
+import '../assets/styles/AccountAccess.css'
+import Logo from './Logo';
 
 function AccountAccess() {
+  // State to manage which form to display
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  // Function to toggle between login and sign up forms
+  const toggleForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
   return (
-    <div>AccountAccess</div>
-  )
+    <div>
+      {showLoginForm ? (
+        <LoginForm toggleForm={toggleForm} />
+      ) : (
+        <SignUpForm toggleForm={toggleForm} />
+      )}
+    </div>
+  );
 }
 
-export default AccountAccess
+// Login form component
+function LoginForm({ toggleForm }) {
+  return (
+    <div className='form-container'>
+      <Logo />
+      {/* TODO: add onSubmit="" to form */}
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" placeholder='E.g. Steve Jobs' autoComplete="username" required/>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder='************' autoComplete="current-password" required />
+        <button type="submit">Login</button>
+      </form>
+      <button className="switch-form" onClick={toggleForm}>Not got an account?</button>
+    </div>
+  );
+}
+
+// Sign up form component
+function SignUpForm({ toggleForm }) {
+  return (
+    <div className='form-container'>
+     <Logo />
+       {/* TODO: add onSubmit="" to form */}
+      <form>
+        <label htmlFor="fullname">Full Name</label>
+        <input type="text" id="fullname" name="fullname" autoComplete="name" required />
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" autoComplete="username" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" autoComplete="new-password" required  />
+        <button type="submit">Sign Up</button>
+      </form>
+      <button className="switch-form" onClick={toggleForm}>Already have an account?</button>
+    </div>
+  );
+}
+
+export default AccountAccess;
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import dayjs from 'dayjs';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Overview() {
+const Overview = () => {
+  const userName = 'John';
+  const currentDate = dayjs().format('ddd D MMMM YYYY');
+
   return (
-    <div>Overview</div>
-  )
-}
+    <div className="container-fluid bg-light p-3">
+      <div className="row">
+        <div className="col">
+          <p className="m-0">Welcome back, {userName}!</p>
+        </div>
+        <div className="col text-end">
+          <p className="m-0">{currentDate}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Overview
+export default Overview;
