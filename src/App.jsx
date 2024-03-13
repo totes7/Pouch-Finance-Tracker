21821560659bd7551a70dc77b9b9605388bbe821
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-03-13 13:46:10 +0000

    add Documents component, unstyled

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Documents from './components/Documents'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function Documents() {
   return (
-    <div>Documents</div>
+    <>
+    <div className="container">
+      <h5 className="title">All Documents</h5>
+      <div className="card">
+        <div className="card-body">
+        <i className="fa-solid fa-receipt"></i>
+        <h5 className="card-title">iPhone 15</h5>
+        <p className="card-text">Bills</p>
+        <button type="button" className="btn btn-success">View</button>
+        <button type="button" className="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <div className="card">
+        <div className="card-body">
+        <i className="fa-solid fa-briefcase"></i>
+        <h5 className="card-title">Salary</h5>
+        <p className="card-text">Income</p>
+        <button type="button" className="btn btn-success">View</button>
+        <button type="button" className="btn btn-danger">Delete</button>
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
 
