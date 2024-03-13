217cee727a0de7ef868d3861ee02592fd85b750e
nolythedev <nolychigwedere@hotmail.co.uk>
2024-03-13 18:42:09 +0000

    Form component

---
+++ src/utils/TransactionTypes.js
@@ -0,0 +1,37 @@
+export const TransactionTypes = [
+    {
+        id: 1,
+        title: 'Shopping',
+        icon: '<i class="fa-solid fa-basket-shopping"></i>'
+    },
+    {
+        id: 2,
+        title: 'Food and Drink',
+        icon: '<i class="fa-solid fa-utensils"></i>'
+    },
+    {
+        id: 3,
+        title: 'Entertainment',
+        icon: '<i class="fa-solid fa-headphones"></i>'
+    },
+    {
+        id: 4,
+        title: 'Bills',
+        icon: '<i class="fa-solid fa-receipt"></i>'
+    },
+    {
+        id: 5,
+        title: 'Savings',
+        icon: '<i class="fa-solid fa-piggy-bank"></i>'
+    },
+    {
+        id: 6,
+        title: 'Income',
+        icon: '<i class="fa-solid fa-briefcase"></i>'
+    },
+    {
+        id: 7,
+        title: 'Savings',
+        icon: '<i class="fa-solid fa-briefcase"></i>'
+    },
+]
