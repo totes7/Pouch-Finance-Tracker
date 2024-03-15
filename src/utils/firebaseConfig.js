4325d51b360900e38017afc92ab983c1f41edf0e
nolythedev <nolychigwedere@hotmail.co.uk>
2024-03-15 17:26:28 +0000

    New files added

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1lhXzkwzslSudFPed58CVXr9i6JRLNqE",
+    authDomain: "pouch-90c28.firebaseapp.com",
+    projectId: "pouch-90c28",
+    storageBucket: "pouch-90c28.appspot.com",
+    messagingSenderId: "1055575224621",
+    appId: "1:1055575224621:web:f7bdc9b4e8b5c7b46b63a4",
+  };
+  
+  export default firebaseConfig;
