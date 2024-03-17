10e18446a22496b95c36c99993bda4989dac56a2
nolythedev <nolychigwedere@hotmail.co.uk>
2024-03-17 10:04:24 +0000

    Overview styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import '../assets/styles/Footer.css'
+import dayjs from 'dayjs';
 
 function Footer() {
+    let currentYear = dayjs().year();
+
   return (
-    <div>Footer</div>
+    <div className='footer-wrapper'>
+        <div className="footer-details">
+            <p>{currentYear} &copy; Pouch</p>
+            <div className="icons-wrapper">
+                <button><i className="fa-brands fa-github"></i></button>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
