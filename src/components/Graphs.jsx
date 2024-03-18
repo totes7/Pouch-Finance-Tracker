15cb0f339fb3824616ba2f42f434764ed3798f91
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-03-18 17:16:52 +0000

    add updated graphs and styling

--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from "react";
 import { DoughnutChart } from "./DoughnutChart";
 import { PieChart } from "./PieChart";
+import "../assets/styles/Graphs.css";
 
 function Graphs() {
   return (
     <>
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title text-dark">Total Income</h5>
-            <PieChart />
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title text-dark">Total Expenses</h5>
-            <DoughnutChart />
+      <div className="graphs-container">
+        <div className="row row-cols-4">
+          <div className="card-container col-5">
+            <div className="card pie">
+              <div className="card-body">
+                <h5 className="card-title pie">Total Income</h5>
+                <PieChart />
+              </div>
+            </div>
+            <div className="card-container col">
+              <div className="card doughnut">
+                <div className="card-body">
+                  <h5 className="card-title doughnut">Total Expenses</h5>
+                  <DoughnutChart />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-    labels: ['Salary', 'Savings', 'Emergency Fund'],
-    datasets: [
-        {
-            label: '£',
-            data: [2500, 450, 100],
-            backgroundColor: [
-                'rgba(70, 174, 134, 0.5)',
-                'rgba(191, 204, 202, 0.5)',
-                'rgba(26, 78, 47, 0.5)',
+  labels: ["Salary", "Savings", "Emergency Fund"],
+  datasets: [
+    {
+      label: "£",
+      data: [2500, 450, 100],
+      backgroundColor: [
+        "rgba(70, 174, 134, 0.5)",
+        "rgba(191, 204, 202, 0.5)",
+        "rgba(26, 78, 47, 0.5)",
+      ],
+      borderColor: [
+        "rgba(70, 174, 134, 0.5)",
+        "rgba(191, 204, 202, 0.5)",
+        "rgba(26, 78, 47, 0.5)",
+      ],
+      borderWidth: 1,
+      hoverOffset: 40,
+    },
+  ],
+};
 
+export const options = {
+  maintainAspectRatio: false,
+  legend: {
+    position: "bottom",
+  }
+};
 
-
-            ],
-            borderColor: [
-                'rgba(70, 174, 134, 0.5)',
-                'rgba(191, 204, 202, 0.5)',
-                'rgba(26, 78, 47, 0.5)',
-
-            ],
-            borderWidth: 1,
-            hoverOffset: 40,
-        },
-    ]
-}
-
- export function PieChart() {
-    return <Pie data={data} />;
+export function PieChart() {
+  return (
+    <div className="pie canvas-container">
+      <Pie
+        data={data}
+        width={50}
+        height={150}
+        options={options}
+      />
+    </div>
+  );
 }
