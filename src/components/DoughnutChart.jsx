aaccc0f11dc3c61fc36f059a90c587fa77e0e363
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-03-18 17:20:32 +0000

    remove doughnut css

--- src/components/DoughnutChart.jsx
+++ src/components/DoughnutChart.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
-import "../assets/styles/DoughnutChart.css";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
